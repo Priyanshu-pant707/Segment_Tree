6382b016e8c6d9b55c1c20ae7a5c298d6129a933
Escape <Escape>
2025-06-30 19:38:40 +0530

    build and update segment tree

---
+++ QueryUpdate.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void buildTree(vector<int> &arr, int index, int left, int right, vector<int> &segmentTree)
+{
+    if (left == right)
+    {
+        segmentTree[index] = arr[left];
+        return;
+    }
+
+    int mid = (left + right) / 2;
+
+    buildTree(arr, 2 * index + 1, left, mid, segmentTree);
+    buildTree(arr, 2 * index + 2, mid + 1, right, segmentTree);
+    segmentTree[index] = segmentTree[2 * index + 1] + segmentTree[2 * index + 2];
+}
+
+void updateTREE(vector<int> &segmentTree, int index, int val, int i, int left, int right)
+{
+    // index is the index of rth
+    if (left == right)
+    {
+        segmentTree[i] = val;
+        return;
+    }
+    int mid = (left + right) / 2;
+
+    if (index <= mid)
+    {
+        updateTREE(segmentTree, index, val, 2 * i + 1, left, mid);
+    }
+    else
+    {
+        updateTREE(segmentTree, index, val, 2 * i + 2, mid + 1, right);
+    }
+    segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+}
+
+int main()
+{
+    int n;
+    cout << "enter the size of the array :";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "enter the elements  of the array : " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> segmentTree(4 * n, 0);
+
+    cout << "Calling the build segment tree function ....." << endl;
+    buildTree(arr, 0, 0, n - 1, segmentTree);
+
+    cout << "Your segment tree is ready to use ..." << endl;
+
+
+    cout<<"before updating : ";
+    for(auto it:segmentTree)
+    cout<<it<<" ";
+    cout<<endl;
+    cout << "Now enter you query for updating (index and value) :";
+    int idx, val;
+    cin >> idx >> val;
+
+    cout << "Updating the Segment tree......." << endl;
+    updateTREE(segmentTree, idx, val, 0, 0, n - 1);
+    cout<<"updation done..."<<endl;
+
+        cout<<"after updating : ";
+    for(auto it:segmentTree)
+    cout<<it<<" ";
+}
---
+++ buildTree.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void buildTree(vector<int> &arr, int index, int left, int right, vector<int> &segTree)
+{
+    if (left == right)
+    {
+        segTree[index] = arr[right];
+        return;
+    }
+
+    int mid = (left + right) / 2;
+
+    buildTree(arr, 2 * index + 1, left, mid, segTree);
+    buildTree(arr, 2 * index + 2, mid + 1, right, segTree);
+    segTree[index] = segTree[2 * index + 1] + segTree[2 * index + 2];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Size of the Array :";
+    cin >> n;
+
+    vector<int> segmentTree(4 * n, 0);
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    buildTree(arr, 0, 0, n - 1, segmentTree);
+
+    for (auto it : segmentTree)
+    {
+        cout << it<<" " ;
+    }
+}
